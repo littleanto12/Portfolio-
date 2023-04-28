69e7d8ba2392f04567ae89e6e7bb534a4234a609
Antoinette simbi <antoinette.simbi@go.minneapolis.ed>
2023-04-28 03:01:49 -0500

    changed the layout of everything

--- src/app.js
+++ src/app.js
@@ -1,8 +1,59 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+let burger = document.querySelector(".burger");
+let navMenu = document.querySelector(".group .navigation");
 
-function toggleButton () {
-    navList.classList.toggle ('show')
+burger.addEventListener("click", () =>{
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+
+/*Lightbox */
+
+
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function openLightbox() {
+  document.getElementById('Lightbox').style.display = 'block';
 }
 
-hamburgerButton.addEventListener ('click', toggleButton)
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+}
+
+function changeSlide(n) {
+	showSlide(slideIndex += n);
+}
+
+function toSlide(n) {
+	showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+
+  const slides = document.getElementsByClassName('slide');
+  let modalPreviews = document.getElementsByClassName('modal-preview');
+
+  if (n > slides.length) {
+    slideIndex = 1;	
+  }
+  
+  if (n < 1) {
+  	slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < modalPreviews.length; i++) {
+      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  }
+  
+  slides[slideIndex - 1].style.display = 'block';
+  modalPreviews[slideIndex - 1].className += ' active';
+}
